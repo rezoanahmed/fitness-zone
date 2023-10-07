a4b9e5f42b45e709ed5a0d6dd2da4693068143be
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-07 17:39:08 +0600

    created hero section according to the details

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 const Hero = () => {
     return (
         <div>
-            <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
-                <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero h-[70vh]" style={{ backgroundImage: 'url(https://i.ibb.co/fvb0DTx/body.jpg)' }}>
+                <div className="hero-overlay bg-black bg-opacity-70"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <h1 className="mb-5 text-3xl font-bold">Unleash Your Inner Strength in Every Rep, Every Step!</h1>
+                        <p className="mb-5">Transform Your Mind & Body. <br />
+
+                            Dedicated Trainers, Top-Notch Equipment. <br />
+                            Join Us and Ignite Your Fitness Journey!</p>
+                        <button className="bg-red-600 font-medium p-2 rounded-xl">Join Us Today!</button>
                     </div>
                 </div>
             </div>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,10 +3,17 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import Root from "../root/Root";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+    ]
   },
 ]);
