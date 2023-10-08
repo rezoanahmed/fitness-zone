d630b640b2af5a992b31e05e459f60d9a5482509
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-08 23:52:37 +0600

    some styles changed in marquee

--- src/pages/Clients/Client.jsx
+++ src/pages/Clients/Client.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const Client = ({client}) => {
-    const {name, message, image} = client
+const Client = ({ client }) => {
+    const { name, message, image } = client
     return (
         <div>
-            <div className='w-[250px] flex flex-col justify-center items-center gap-3 text-center'>
+            <div className='p-4 w-[250px] flex flex-col justify-center items-center gap-3 text-center'>
                 <img src={image} alt={image} className='h-[200px] w-[200px] rounded-full' />
                 <p className='font-semibold text-xl'>{name}</p>
-                <p className='text-sm'>{message}</p>
+
+                <p className='text-sm font-gray-600 italic'>{`"${message}"`}</p>
+
             </div>
         </div>
     );
