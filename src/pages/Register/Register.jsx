2a6759ee95a56d6b4775f3e0f4bdfe8ff74f369d
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-08 20:54:13 +0600

    registration page problem solved

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -95,8 +95,8 @@ const Register = () => {
 
             <h1 className='text-2xl font-semibold text-center my-5' data-aos="zoom-out">Register today and start your journey!</h1>
             <div className=' flex flex-col items-center justify-center' data-aos="fade-up">
-                <div className="card shadow-2xl bg-base-100">
-                    <form onSubmit={register} className="card-body">
+                <div className="card shadow-2xl bg-base-100 card-body">
+                    <form onSubmit={register} className="">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
@@ -119,13 +119,14 @@ const Register = () => {
                         <div className="form-control mt-6">
                             <button className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600">Register</button>
                         </div>
-                        <div className="form-control mt-1">
-                            <button onClick={handleGoogleLogin} className="flex items-center justify-center gap-2 border px-4 py-2 rounded-lg hover:bg-blue-50"><FcGoogle className="text-2xl"></FcGoogle>Get Started with Google</button>
-                        </div>
-                        <div className='mt-2'>
-                            Already Have Account? <Link to='/login'><span className='text-red-500 font-medium'>Login Now!</span></Link>
-                        </div>
+                        
                     </form>
+                    <div className="mt-1">
+                            <button onClick={handleGoogleLogin} className="flex items-center justify-center gap-2 border px-4 py-2 rounded-lg hover:bg-blue-50 w-full"><FcGoogle className="text-2xl"></FcGoogle>Get Started with Google</button>
+                        </div>
+                        <div className='mt-2 '>
+                            <p>Already Have Account? <Link to='/login'><span className='text-red-500 font-medium'>Login Now!</span></Link></p>
+                        </div>
                 </div>
                             {passwordError && <p className="text-red-500 mt-2">{passwordError}</p>}
             </div>
