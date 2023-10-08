3ca068fce237aafd516aaaa55ea87ea1a7dbaefd
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-09 00:33:33 +0600

    some minor errors has been resolved

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
                 <nav className="grid md:grid-flow-col gap-4">
                     <Link to='/services'>Services</Link>
-                    <Link to='/trainers'>Trainers</Link>
+                    <Link to='/sponsors'>Sponsors</Link>
                     <Link to='/plans'>Supplements Plans</Link>
                     <Link to='/about'>About</Link>
                     <Link to='/contact'>Contact</Link>
@@ -19,7 +19,7 @@ const Footer = () => {
                     </div>
                 </nav>
                 <aside>
-                    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+                    <p>Copyright © 2023 - All right reserved by Fitness Zone Ltd</p>
                 </aside>
             </footer>
         </div>
