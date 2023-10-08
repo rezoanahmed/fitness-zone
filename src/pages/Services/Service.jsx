6ca7f9a3f14f2f41cab6329635641ee770f8cbf4
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-08 16:28:29 +0600

    added aos animation to services page

--- src/pages/Services/Service.jsx
+++ src/pages/Services/Service.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Service = ({service}) => {
+const Service = ({service, ...props}) => {
     const {id, title, price, image} = service;
     return (
-        <div>
+        <div {...props}>
             <div className='space-y-2 flex flex-col justify-center items-center p-4 shadow-xl bg-base-100 rounded-md'>
                 <img src={image} alt="" className='h-[250px] w-[450px] rounded-md'/>
                 <p className='text-xl font-medium'>{title}</p>
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,21 +1,34 @@
 import { useEffect, useState } from "react";
 import Service from "./Service";
-
+import Aos from "aos";
+import 'aos/dist/aos.css'
 
 const Services = () => {
+    // animation
+    useEffect(() => {
+        Aos.init({
+            duration: 1500
+        })
+    }, [])
     const [service, setService] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('/gym.json')
-        .then(res=>res.json())
-        .then(data=>setService(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [])
     return (
         <>
-        <h1 className="my-5 text-4xl font-semibold text-center">Our Packages</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-            {/* {service.length} */}
-            {service.map(element=><Service key={element.id} service={element}></Service>)}
-        </div>
+            <h1 className="my-5 text-4xl font-semibold text-center">Our Packages</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                {/* {service.length} */}
+                {service.map((element, index) => <Service
+                    key={element.id}
+                    service={element}
+                    data-aos="zoom-in-up"
+                    data-aos-delay={`${index * 100}`}>
+
+                </Service>)}
+            </div>
         </>
     );
 };
